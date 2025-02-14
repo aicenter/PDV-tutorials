21a0fc65f5ec6de163f69a4ee51d69736b7eff17
Jakub Dupak <dev@jakubdupak.com>
2025-02-14 19:57:48 +0100

    Update build scripts

--- support/scripts/build.sh
+++ support/scripts/build.sh
@@ -16,12 +16,19 @@ function build_latex {
 	(cd $ROOT/$1 && TEXINPUTS=.//:$ROOT:$ROOT/support/beamer-template:$TEXINPUTS lualatex -shell-escape $ROOT/$1.tex)
 }
 
+function build_typst {
+	(cd $ROOT/$1 && typst compile --root $ROOT main.typ $1.pdf)
+}
+
 function build_cmake {
 	(cd $ROOT/$1 && cmake CMakeLists.txt && make $PARAMS)
 }
 
 while [ ! $RELATIVE == "." ]; do
-	if [ -f $ROOT/$RELATIVE/main.tex ]; then
+	if [ -f $ROOT/${RELATIVE:0:-1}/main.typ ] ; then       
+		build_typst ${RELATIVE:0:-1}
+		exit 0
+	elif [ -f $ROOT/$RELATIVE/main.tex ]; then
 		build_ipe $RELATIVE
 		build_latex $RELATIVE
 		exit 0
@@ -31,7 +38,7 @@ while [ ! $RELATIVE == "." ]; do
     elif [ -f $ROOT/${RELATIVE:0:-1}.tex ] ; then
         build_ipe $RELATIVE
         build_latex ${RELATIVE:0:-1}
-        exit 0        
+        exit 0
 	fi
 	RELATIVE=$(dirname $RELATIVE)
 done
